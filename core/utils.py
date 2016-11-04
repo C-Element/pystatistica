1ee3833c64810b445de12d4641369ff13ff93fd8
Clemente Junior <clmnt.jr@gmail.com>
2016-11-04 09:57:00 -0300

    ADD: fi and Fi to StatisticClass

--- core/utils.py
+++ core/utils.py
@@ -2,15 +2,33 @@ import math
 
 
 class StatisticClass(object):
-    def __init__(self, min_range, max_range, data):
+    def __init__(self, min_range, max_range, data, complete_data):
+        if not isinstance(min_range, int):
+            raise TypeError('"min_range" value need be int.')
+        if not isinstance(max_range, int):
+            raise TypeError('"max_range" value need be int.')
         if not isinstance(data, list):
             raise TypeError('"data" value need be list.')
+        if not isinstance(complete_data, tuple):
+            raise TypeError('"complete_data" value need be tuple.')
         self.data = data
+        self.complete_data = complete_data
         self.min = min(data)
         self.min_range = min_range
         self.max = max(data)
         self.max_range = max_range
         self.data.sort()
+
+    @property
+    def fi(self):
+        return len(self.data)
+
+    @property
+    def Fi(self):
+        count = 0
+        while count < len(self.complete_data) and self.complete_data[count] < self.max_range:
+            count += 1
+        return count
 
     @property
     def name(self):
@@ -39,6 +57,7 @@ def create_class_from_bytes(bytes):
     max_actual = min_data + amplitude
     min_actual = min_data
     data.sort()
+    complete_data = tuple(data.copy())
     actual = data[0]
     while len(data) != 0:
         this_data = []
@@ -46,7 +65,7 @@ def create_class_from_bytes(bytes):
             this_data.append(data.pop(0))
             if len(data) > 0:
                 actual = data[0]
-        result.append(StatisticClass(min_actual, max_actual, this_data))
+        result.append(StatisticClass(min_actual, max_actual, this_data, complete_data))
         min_actual = max_actual
         max_actual += amplitude
     return result
